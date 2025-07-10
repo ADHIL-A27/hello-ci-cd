02d6a9a3b0bd6f576c90ff3f5847437f4bbcfe0b
MOHAMMED ADHIL <122203370+ADHIL-A27@users.noreply.github.com>
2025-07-10 12:17:10 +0530

    Test CI/CD

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🚀 Hello CI/CD World!"
+    return "🚀 CI/CD is working!"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # use Render's assigned port
